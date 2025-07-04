e1b3a1ea9bedfb2cb60f879555af7a5990f99545
db <db@overmindgroup.com>
2025-07-04 16:46:34 +0300

    project and packages

---
+++ ZooProject/Assets/ExternalPackages/UnityMessagingSystem/MessagingSubscriberExample.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace JGM.MessagingSystem
+{
+    public class MessagingSubscriberExample : MonoBehaviour,
+        IMessagingSubscriber<PlayerSpawnMessage>,
+        IMessagingSubscriber<PlayerDeathMessage>
+    {
+        private void OnEnable()
+        {
+            MessagingSystem.Instance.Subscribe<PlayerSpawnMessage>(this);
+            MessagingSystem.Instance.Subscribe<PlayerDeathMessage>(this);
+        }
+
+        private void OnDisable()
+        {
+            MessagingSystem.Instance.Unsubscribe<PlayerSpawnMessage>(this);
+            MessagingSystem.Instance.Unsubscribe<PlayerDeathMessage>(this);
+        }
+
+        public void OnReceiveMessage(PlayerSpawnMessage message)
+        {
+            Debug.Log(message.ToString());
+        }
+
+        public void OnReceiveMessage(PlayerDeathMessage message)
+        {
+            Debug.Log("Received Player Death message");
+        }
+    }
+}
